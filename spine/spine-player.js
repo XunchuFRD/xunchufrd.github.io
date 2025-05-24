abc7dfcf48aef2410285e04f8f5bd93e7acf5ee7
CnSyw <2718492970@qq.com>
2025-05-24 12:12:16 +0800

    Site updated: 2025-05-24 12:12:08

--- spine/spine-player.js
+++ spine/spine-player.js
@@ -1,5 +1,5 @@
 // 全局配置
-const spine_model_path = "https://xunchufrd.github.io/blog/spine/models/";
+const spine_model_path = "https://xunchufrd.github.io/spine/models/";
 var MODELS = [ // 模型列表
     "build_char_017_huang_witch_5",
     "build_char_017_huang", // 可用模型列表
